05b72dce2219e64d62e432c8395ea32506c18794
abhiyanbeta <abhiyanbeta@gmail.com>
2022-10-06 12:56:07 +0100

    initital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '2-cards';
+  posts = [
+    { 
+      title: 'Neat tree',
+      imageUrl: 'assets/tree.jpg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    { 
+      title: 'Snowy mounthat',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover5',
+      content: 'heres a pic of a snowy mountain'
+    },
+    { 
+      title: 'mountain biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biker1212',
+      content: 'I did some biking today'
+    }
+  ];
 }
